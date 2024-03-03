cc6d610f4b9513c2ba178d8445ee2a042cde964e
Alex Bazhenov <vorobushec13@gmail.com>
2024-03-03 16:56:17 +0300

    New update

--- calcul_test.go
+++ calcul_test.go
@@ -50,7 +50,7 @@ func Test_infixToRPN(t *testing.T) {
 			input:    "-10 + 12 )",
 			expected: 0,
 			wantErr:  true,
-		}, //////////////////////////
+		},
 		{
 			input:    "10 + 12 ( )",
 			expected: 0,
@@ -103,22 +103,4 @@ func Test_infixToRPN(t *testing.T) {
 			t.Error("input: ", tt.input, "expected: ", tt.expected, "result: ", result)
 		}
 	}
-
-	// много скобок ( ( 2 - 3 * 3 ) * 2 - ( ( 2 * 1 ) * ( 12 / 4 ) ) ) * 1 true
-	// если один символ 5 true
-	// - один цифра в начале true
-	// 0 true
-	// без пробелов true
-	// делить на 0 true
-	// в конце неверный символ 10 - 12 - true
-	// в конце скобка неверная скобка -10 + 12 ) true
-	// в конце пробел true
-	// 10 + 12 ( ) true
-	// ssd + 2 true
-
-	// пробел в начале " 12 - 1" true
-	//  12 - ( ) 2 true
-	// 12 - ( ) / 2 true
-	// - ( 9 - 1 ) true
-	// ( 12 ) - 2 true
 }
